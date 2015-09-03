a4f678b265493b5d27d13adacfd4448955887380
Zeke <saravanabalagi@hotmail.com>
2015-09-03 18:11:42 +0530

    Resend Otp done

--- app/src/main/java/in/foodmash/app/ForgotPasswordOtpActivity.java
+++ app/src/main/java/in/foodmash/app/ForgotPasswordOtpActivity.java
@@ -38,6 +38,7 @@ public class ForgotPasswordOtpActivity extends AppCompatActivity implements View
     TextView otpTime;
     TextView otpInfo;
     String recoveryKey=null;
+    String type;
 
     Handler handler=new Handler();
     int timerMinutes=3;
@@ -82,8 +83,9 @@ public class ForgotPasswordOtpActivity extends AppCompatActivity implements View
         otpInfo = (TextView) findViewById(R.id.otp_info);
 
         recoveryKey=getIntent().getStringExtra("value");
-        if(getIntent().getStringExtra("type").equals("email")) otpInfo.setText("We have sent an OTP (One Time Password) to your email '"+recoveryKey+"', enter it below to reset your account password. You can resend the OTP once the timer expires.");
-        else if(getIntent().getStringExtra("type").equals("phone")) otpInfo.setText("We have sent an OTP (One Time Password) to your phone "+"+91"+recoveryKey+" via a private message, enter it below to reset your account password. You can resend the OTP once the timer expires.");
+        type = getIntent().getStringExtra("type");
+        if(type.equals("email")) otpInfo.setText("We have sent an OTP (One Time Password) to your email '"+recoveryKey+"', enter it below to reset your account password. You can resend the OTP once the timer expires.");
+        else if(type.equals("phone")) otpInfo.setText("We have sent an OTP (One Time Password) to your phone "+"+91"+recoveryKey+" via a private message, enter it below to reset your account password. You can resend the OTP once the timer expires.");
 
         handler.removeCallbacks(initiateOtpTimer);
         handler.postDelayed(initiateOtpTimer, 1000);
@@ -92,14 +94,7 @@ public class ForgotPasswordOtpActivity extends AppCompatActivity implements View
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.resend_otp:
-                otpExpired = false;
-                timerMinutes = 1; timerSeconds = 0;
-                Animations.fadeOutAndFadeIn(otpExpiredLayout,otpTimeLayout,500);
-                Animations.fadeOutAndFadeIn(resendOtp, otpFillLayout, 500);
-                handler.removeCallbacks(initiateOtpTimer);
-                handler.postDelayed(initiateOtpTimer, 1000);
-                break;
+            case R.id.resend_otp: if(otpExpired) resendOtpRequest(); break;
             case R.id.back: intent = new Intent(this, ForgotPasswordActivity.class); startActivity(intent); break;
             case R.id.proceed: if(isEverythingValid()) makeRequest(); else Alerts.validityAlert(ForgotPasswordOtpActivity.this); break;
         }
@@ -165,6 +160,49 @@ public class ForgotPasswordOtpActivity extends AppCompatActivity implements View
     private boolean isEverythingValid() {
         return !otpExpired &&
                 otp.getText().toString().trim().length()==6;
+    }
+
+    private JSONObject getOtpRequestJson() {
+        JSONObject requestJson = new JSONObject();
+        try {
+            requestJson.put("android_id", Settings.Secure.getString(this.getContentResolver(),Settings.Secure.ANDROID_ID));
+            JSONObject userJson = new JSONObject();
+            if(type.equals("phone")) userJson.put("mobile_no",recoveryKey);
+            else if(type.equals("email")) userJson.put("email",recoveryKey);
+            JSONObject dataJson = new JSONObject();
+            dataJson.put("user",userJson);
+            requestJson.put("data",dataJson);
+        } catch (JSONException e) { e.printStackTrace(); }
+        return requestJson;
+    }
+
+    private void resendOtpRequest() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, getString(R.string.api_root_path) + "/registrations/forgotPassword", getOtpRequestJson(), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    if(response.getBoolean("success")) {
+                        otpExpired = false;
+                        timerMinutes = 3; timerSeconds = 0;
+                        Animations.fadeOutAndFadeIn(otpExpiredLayout, otpTimeLayout, 500);
+                        Animations.fadeOutAndFadeIn(resendOtp, otpFillLayout, 500);
+                        handler.removeCallbacks(initiateOtpTimer);
+                        handler.postDelayed(initiateOtpTimer, 1000);
+                    } else if(!(response.getBoolean("success"))) {
+                        Alerts.commonErrorAlert(ForgotPasswordOtpActivity.this, "Address Invalid", "We are unable to process your Address Details. Try Again!", "Okay");
+                        System.out.println("Error: " + response.getString("error"));
+                    }
+                } catch (JSONException e) { e.printStackTrace(); }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if(error instanceof NoConnectionError || error instanceof TimeoutError) Alerts.internetConnectionErrorAlert(ForgotPasswordOtpActivity.this);
+                else Alerts.unknownErrorAlert(ForgotPasswordOtpActivity.this);
+                System.out.println("JSON Error: " + error);
+            }
+        });
+        Swift.getInstance(ForgotPasswordOtpActivity.this).addToRequestQueue(jsonObjectRequest);
     }
 
 }
