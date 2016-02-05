766973e1a3353ca727ea7a84acc1332b7280cb51
zekedran <saravanabalagi@hotmail.com>
2016-02-05 13:41:55 +0530

    Added check hash before replacing functionality

--- app/src/main/java/in/foodmash/app/custom/Cache.java
+++ app/src/main/java/in/foodmash/app/custom/Cache.java
@@ -9,10 +9,8 @@ public class Cache {
     private static List<Combo> combos;
 
     public static List<Combo> getCombos() { return combos; }
-    public static Combo getCombo(int comboId) {
-        System.out.println("ComboID: "+comboId);
-        for (Combo c : combos) if (c.getId()==comboId) return c;
-        return null;
-    }
-    public static void setCombos(List<Combo> combosParams) { combos = combosParams; }
+    public static Combo getCombo(int comboId) { for (Combo c : combos) if (c.getId()==comboId) return c; return null; }
+
+    public static void setCombos(List<Combo> combosParams) { if(isNew(combosParams)) combos = combosParams; }
+    private static boolean isNew(List<Combo> combosParams) { return combos == null || combosParams.hashCode() != combos.hashCode(); }
 }
