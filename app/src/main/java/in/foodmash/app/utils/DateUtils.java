ee9c59e202e42bb38ab8f2c9d9258dd49a687514
zekedran <saravanabalagi@hotmail.com>
2016-03-17 05:21:16 +0530

    Added additional date calc functions

--- app/src/main/java/in/foodmash/app/utils/DateUtils.java
+++ app/src/main/java/in/foodmash/app/utils/DateUtils.java
@@ -6,13 +6,14 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Zeke on Oct 27 2015.
  */
 public class DateUtils {
 
-    public static String railsDateToLocalTime(String railsUtcDateFormat) {
+    public static String railsDateStringToReadableTime(String railsUtcDateFormat) {
         try {
             DateFormat utcDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault());
             utcDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
@@ -21,6 +22,22 @@ public class DateUtils {
             istDateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Kolkata"));
             return istDateFormat.format(date);
         } catch (ParseException e) { e.printStackTrace(); return null; }
+    }
+
+    public static Date railsDateStringToJavaDate(String railsUtcDateFormat) {
+        try {
+            DateFormat utcDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault());
+            utcDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+            return utcDateFormat.parse(railsUtcDateFormat);
+        } catch (ParseException e) { e.printStackTrace(); return null; }
+    }
+
+    public static long howOldInHours(Date date) { return TimeUnit.MILLISECONDS.toHours(new Date().getTime() - date.getTime()); }
+    public static long howOldInDays(Date date) { return TimeUnit.MILLISECONDS.toDays(new Date().getTime() - date.getTime()); }
+    public static String javaDateToRailsDateString(Date date) {
+        DateFormat utcDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault());
+        utcDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return utcDateFormat.format(date);
     }
 
 }
