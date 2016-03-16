c83c77f7f7d6c758da6b9083fd8d054a31439be0
zekedran <saravanabalagi@hotmail.com>
2016-03-17 05:19:44 +0530

    New snackbar for combo not in server error

--- app/src/main/java/in/foodmash/app/CartActivity.java
+++ app/src/main/java/in/foodmash/app/CartActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -87,6 +88,14 @@ public class CartActivity extends AppCompatActivity implements View.OnClickListe
             totalErrorSnackbar.setAction("Try Again", new View.OnClickListener() { @Override public void onClick(View v) { totalErrorSnackbar.dismiss(); } });
             totalErrorSnackbar.show();
         }
+
+        if(getIntent().getBooleanExtra("combo_error",false)) {
+            final Snackbar invalidCombos = Snackbar.make(mainLayout, "Combos that are not currently available are removed.", Snackbar.LENGTH_INDEFINITE);
+            invalidCombos.setAction("Try Again", new View.OnClickListener() { @Override public void onClick(View v) { invalidCombos.dismiss(); } });
+            invalidCombos.show();
+        }
+
+
 
         buy.setOnClickListener(this);
         fillLayout = (LinearLayout) findViewById(R.id.fill_layout);
