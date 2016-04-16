feabe978f652af2452dfa2e46942bd538c1501e1
zekedran <saravanabalagi@hotmail.com>
2016-04-17 00:09:12 +0530

    Added bottomMargin for last item in recyclerView

--- app/src/main/java/in/foodmash/app/AddressActivity.java
+++ app/src/main/java/in/foodmash/app/AddressActivity.java
@@ -221,6 +221,12 @@ public class AddressActivity extends FoodmashActivity implements View.OnClickLis
                     Swift.getInstance(AddressActivity.this).addToRequestQueue(deleteRequest);
                 }
             });
+
+            if(position == this.getItemCount()-1) {
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                layoutParams.setMargins(dpToPx(10),dpToPx(10),dpToPx(10),dpToPx(40));
+                viewHolder.itemView.setLayoutParams(layoutParams);
+            }
         }
     }
 
